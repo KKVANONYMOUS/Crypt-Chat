0133538af5611021c3c4e2b18e8d6eae67e48e2d
kkvanonymous <ninja.kunal01@gmail.com>
2021-04-09 17:04:08 +0530

    Complete Profile view

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -10,11 +10,13 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
+  final scaffoldKey=GlobalKey<ScaffoldState>();
   DatabaseMethods databaseMethods = new DatabaseMethods();
   QuerySnapshot searchUserSnapshot;
   TextEditingController NameEditingController = new TextEditingController();
   TextEditingController BioEditingController = new TextEditingController();
+  bool isNameValid=true;
+  bool isBioValid=true;
 
   @override
   void initState() {
@@ -22,115 +24,130 @@ class _ProfileScreenState extends State<ProfileScreen> {
     .then((val){
       setState(() {
         searchUserSnapshot=val;
+        NameEditingController.text=val.docs[0].data()['name'];
+        BioEditingController.text=val.docs[0].data()['bio'];
       });
     });
     super.initState();
   }
 
   updateUserInfo(){
-    if(NameEditingController.text.isNotEmpty && BioEditingController.text.isNotEmpty){
+    setState(() {
+      NameEditingController.text.trim().length < 3 || NameEditingController.text.trim().isEmpty ? isNameValid=false:isNameValid=true;
+      BioEditingController.text.trim().length > 30 || BioEditingController.text.trim().isEmpty ?isBioValid=false:isBioValid=true;
+    });
+
+    if(isNameValid && isBioValid){
       databaseMethods.updateUserInfo(Constants.currentUser, NameEditingController.text, BioEditingController.text);
-      print('Profile Updated');
+      SnackBar snackBar=SnackBar(content: Text('Profile updated!'));
+      scaffoldKey.currentState.showSnackBar(snackBar);
+      FocusScope.of(context).unfocus();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     Size screenSize=MediaQuery.of(context).size;
-    return searchUserSnapshot!=null?Scaffold(
-      appBar: AppBar(
-        title: Text('Profile', style: TextStyle(
-            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+    return searchUserSnapshot!=null?GestureDetector(
+      onTap:  () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          title: Text('Profile', style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          elevation: 0,
         ),
-        elevation: 0,
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.1,horizontal: 15),
-        child: ListView(
-          children: [
-            Center(
-              child: Stack(
-                children: [
-                  Container(
-                    width: 130,
-                    height: 130,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).scaffoldBackgroundColor),
-                        boxShadow: [
-                          BoxShadow(
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              color: Colors.black.withOpacity(0.1),
-                              offset: Offset(0, 10))
-                        ],
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage("assets/images/user_avatar.png")),
-                    ),
-
-                  ),
-                  Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.05,horizontal: 15),
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
                           border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).scaffoldBackgroundColor,
-                          ),
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        child: Icon(
-                          Icons.camera_alt_rounded,
-                          color: Colors.white,
-                        ),
-                      )),
-                ],
-              ),
-            ),
-            Center(
-              child: Container(
-                child: Text('@${searchUserSnapshot.docs[0].data()['username']}',style: TextStyle(color:MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Constants.kSecondaryColor,fontSize: 15,fontWeight: FontWeight.bold),),
-              ),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
-              child: Column(
-                children: [
-                  ProfileTextField(context,"Name", searchUserSnapshot.docs[0].data()['name'],NameEditingController),
-                  ProfileTextField(context,"Bio", searchUserSnapshot.docs[0].data()['bio'],BioEditingController),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.1),
-                    child: RaisedButton(
-                      onPressed: () {
-                        updateUserInfo();
-                      },
-                      color: MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Constants.kSecondaryColor,
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text(
-                        "UPDATE",
-                        style: TextStyle(
-                            fontSize: 14,
-                            letterSpacing: 2.2,
-                            color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.white:Colors.black87),
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                                offset: Offset(0, 10))
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/user_avatar.png")),
                       ),
+
                     ),
-                  )
-                ],
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                            ),
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          child: Icon(
+                            Icons.camera_alt_rounded,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
+                ),
               ),
-            )
-          ],
+              Center(
+                child: Container(
+                  child: Text('@${searchUserSnapshot.docs[0].data()['username']}',style: TextStyle(color:MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Constants.kSecondaryColor,fontSize: 15,fontWeight: FontWeight.bold),),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
+                child: Column(
+                  children: [
+                    ProfileTextField(context,"Name", "Enter name",NameEditingController),
+                    ProfileTextField(context,"Bio", "Enter bio",BioEditingController),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.1),
+                      child: RaisedButton(
+                        onPressed: () {
+                          updateUserInfo();
+                        },
+                        color: MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Constants.kSecondaryColor,
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Text(
+                          "UPDATE",
+                          style: TextStyle(
+                              fontSize: 14,
+                              letterSpacing: 2.2,
+                              color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.white:Colors.black87),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ):Container(
@@ -138,40 +155,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
       child: Center(child: CircularProgressIndicator()),
     );
   }
-}
 
-
-Widget ProfileTextField(BuildContext context,
-    String labelText, String placeholder, TextEditingController controller) {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 35.0),
-    child: TextField(
-      controller: controller ,
-      decoration: InputDecoration(
-        prefixIcon: Icon(
-          labelText=='Bio'?Icons.info_outline_rounded:Icons.account_circle_rounded,
-          color: MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Colors.grey,
-        ),
-          suffixIcon: IconButton(
-            onPressed: () {
-            },
-            icon: Icon(
-              Icons.edit,
-              color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.grey:Constants.kPrimaryColor,
+  Widget ProfileTextField(BuildContext context,
+      String labelText, String placeholder, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        controller: controller ,
+        style: TextStyle(fontWeight: FontWeight.bold,),
+        decoration: InputDecoration(
+            prefixIcon: Icon(
+              labelText=='Bio'?Icons.info_outline_rounded:Icons.account_circle_rounded,
+              color: MediaQuery.of(context).platformBrightness==Brightness.light ? Constants.kPrimaryColor:Colors.grey,
             ),
-          ),
-          contentPadding: EdgeInsets.only(bottom: 3),
-          labelText: labelText,
-          labelStyle: TextStyle(
-            color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.black45:Colors.grey
-          ),
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          hintText: placeholder,
-          hintStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.black:Colors.white70,
-          )),
-    ),
-  );
+            suffixIcon: IconButton(
+              onPressed: () {
+              },
+              icon: Icon(
+                Icons.edit,
+                color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.grey:Constants.kPrimaryColor,
+              ),
+            ),
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            labelStyle: TextStyle(
+                color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.black45:Colors.grey
+            ),
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            errorText: labelText=='Name'? (isNameValid ? null : "Name too short") : (isBioValid ? null : BioEditingController.text.trim().isEmpty ? "Bio too short":"Bio too long"),
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.normal,
+              color: MediaQuery.of(context).platformBrightness==Brightness.light ? Colors.black:Colors.white70,
+            )),
+      ),
+    );
+  }
 }
+
+
+
